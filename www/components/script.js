ff6fb6b6a73f30ad25bf77ad29d39e220fd8f102
BrunoGattiSalinas <brunogattisalinas@hotmail.com>
2021-11-11 08:58:22 +0900

    Tema Claro e Escuro

--- www/components/script.js
+++ www/components/script.js
@@ -1,180 +1,198 @@
 window.onload = function(){
-  jogoInicio();
+  inicioJogo();
+
+  if(localStorage.getItem(1) == "a"){
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("canvas").style.backgroundColor = "black";
+  };
+  if(localStorage.getItem(1) == "b"){
+    document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("canvas").style.backgroundColor = "white";
+  };
+
+  var estado = "a";
+  localStorage.setItem(1, estado);
+
+  document.querySelector("#subir").addEventListener("click", function(){
+    subir();
+    setTimeout(parar, 1000);
+  });
+  document.querySelector("#descer").addEventListener("click", function(){
+    descer();
+    setTimeout(parar, 1000);
+  });
   document.querySelector("#direita").addEventListener("click", function(){
-     right();
-     setTimeout(pare, 1000);
+    direita();
+    setTimeout(parar, 1000);
   });
-
-   document.querySelector("#esquerda").addEventListener("click", function(){
-     left();
-     setTimeout(pare, 1000);
+  document.querySelector("#esquerda").addEventListener("click", function(){
+    esquerda();
+    setTimeout(parar, 1000);
   });
-
-   document.querySelector("#subir").addEventListener("click", function(){
-     sobe();
-     setTimeout(pare, 1000);
+  document.querySelector("#claro").addEventListener("click", function(){
+    document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("canvas").style.backgroundColor = "white";
+    if(estado == "a"){
+      estado = "b";
+    };
+    localStorage.setItem(1, estado);
   });
-
-   document.querySelector("#descer").addEventListener("click", function(){
-     desce();
-     setTimeout(pare, 1000);
-  });
-
-  document.querySelector("#reset").addEventListener("click", function(){
-     reload();
+  document.querySelector("#escuro").addEventListener("click", function(){
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("canvas").style.backgroundColor = "black";
+    if(estado == "b"){
+      estado = "a";
+    };
+    localStorage.setItem(1, estado);
   });
 }
 
 var personagemObj;
 
-var osbtaculos = [];
+var obstaculo = [];
 
 var pontos;
 
-
-function jogoInicio(){
+function inicioJogo(){
   areaJogo.start();
-  personagemObj = new componentes("#F00", 10, 120, 30, 30);
-  pontos = new componentes("#000", 30, 30, 'Consolas', '30px', 'texto');
-  //osbtaculos = new componentes('yellow', 120, 80, 10, 100);
+  personagemObj = new componente("red", 10, 120, 30, 30);
+  pontos = new componente("red", 20, 30, "Consolas", "17px", "texto");
 }
 
 let areaJogo = {
-   canvas : document.createElement("canvas"),
-   start: function(){
-     this.canvas.height = 300,
-     this.context = this.canvas.getContext("2d");
-     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-     this.frame = 0;
-     this.intervalo = setInterval(jogoAtualizar, 20);
-   },
-   limpa: function(){
-     this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
-   },
-   stop: function(){
-     clearInterval(this.interval);
-   }
+  canvas: document.createElement("canvas"),
+
+  start: function(){
+    this.canvas.height = 300,
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.frame = 0;
+    this.intervalo = setInterval(atualizaAreaJogo, 20);
+  },
+  
+  limpar: function(){
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+
+  parar: function(){
+    clearInterval(this.interval);
+  }
 }
 
 function contarIntervalo(n){
-  if((areaJogo.frame / n ) % 1 == 0) {
+  if((areaJogo.frame / n) % 1 == 0){
     return true;
-  }else {
+  }else{
     return false;
   }
 }
 
-function componentes(cor, x, y, largura, altura, tipo){
-      this.tipo = tipo,
-      this.altura = altura,
-      this.largura = largura,
-      this.texto = 0,
-      this.x = x,
-      this.y = y,
-      this.veloX = 0,
-      this.veloY = 0,
-      this.atualizar = function(){
-      contexto = areaJogo.context;
-      if(this.tipo == 'texto'){
-      contexto.font = this.altura + " " + this.largura;  
+function componente(cor, x, y, altura, largura, tipo){
+  this.tipo = tipo,
+  this.altura = altura,
+  this.largura = largura,
+  this.x = x,
+  this.y = y,
+  this.velocidadeX = 0,
+  this.velocidadeY = 0,
+  this.texto = 0;
+
+  this.atualiza = function(){
+    contexto = areaJogo.context;
+
+    if(this.tipo == "texto"){
+      contexto.font = this.largura + " " + this.altura;
       contexto.fillStyle = cor;
       contexto.fillText(this.texto, this.x, this.y);
-      }else{
+    }else{
       contexto.fillStyle = cor,
       contexto.fillRect(this.x, this.y, this.altura, this.largura);
-      }
-      },
-      this.novaPosicao = function(){
-        this.x += this.veloX;
-        this.y += this.veloY; 
-      },
-      this.colisao = function(obj){
-        let esq = this.x;
-        let dir = this.x + this.largura;
-        let sup = this.y;
-        let inf = this.y + this.altura;
-        
-        let objEsq = obj.x;
-        let objDir = obj.x + obj.altura;
-        let objSup = obj.y;
-        let objInf = obj.y + obj.largura;
+    }
+  },
 
-        let batida = true;
+  this.novaPosicao = function(){
+    this.x += this.velocidadeX;
+    this.y += this.velocidadeY;
+  },
 
-        if(
-          (inf < objSup) || (sup > objInf) ||
-          (dir < objEsq) || (esq > objDir)
-          ){
-          batida = false;
+  this.bater = function(obj){
+    //posição do personagem
+    let esquerda = this.x;
+    let direita = this.x + this.altura;
+    let superior = this.y;
+    let inferior = this.y + this.largura;
+    //posição do obstáculo
+    let objEsquerda = obj.x;
+    let objDireita = obj.x + obj.altura;
+    let objSuperior = obj.y;
+    let objInferior = obj.y + obj.largura;
 
-          }
-          return batida;
-      }
+    let batida = true;
+
+    if(
+      (inferior < objSuperior) || (superior > objInferior) || 
+      (direita < objEsquerda) || (esquerda > objDireita)
+    ){
+      batida = false;
+    }
+    return batida;
+  }
 }
 
-function jogoAtualizar(){
+function atualizaAreaJogo(){
   let x, y;
 
-  for(i = 0 ; i < osbtaculos.length; i++){
-    if(personagemObj.colisao(osbtaculos[i])){
-    areaJogo.stop();
-    return;
-     }
+  for(i = 0; i < obstaculo.length; i++){
+    if(personagemObj.bater(obstaculo[i])){
+      areaJogo.parar();
+
+      return;
+    }
   }
-  
-  areaJogo.limpa();
+
+  areaJogo.limpar();
   areaJogo.frame += 1;
 
-  if(areaJogo.frame == 1 || contarIntervalo(150)) {
-   x = areaJogo.canvas.width;
-   miniAltura = 20;
-   maxAltura = 200; 
-   altura = Math.floor (Math.random() * (maxAltura - miniAltura + 1 ) + miniAltura);
-   minVazio = 50
-   maxVazio = 200;
-   vazio = Math.floor(Math.random() * (maxVazio - minVazio + 1) + minVazio);
-   //y = areaJogo.canvas.height - 200;
-   osbtaculos.push(new componentes('green', x, 0 , altura, 10));
-   osbtaculos.push(new componentes('green', x, altura  + vazio , x - altura - vazio, 10));
+  if(areaJogo.frame == 1 || contarIntervalo(150)){
+    x = areaJogo.canvas.width;
+    minAltura = 20;
+    maxAltura = 200;
+    altura = Math.floor(Math.random() * (maxAltura - minAltura + 1) + minAltura);
+    minVazio = 50;
+    maxVazio = 200;
+    vazio = Math.floor(Math.random() * (maxVazio - minVazio + 1) + minVazio);
+    obstaculo.push(new componente("green", x, 0, 10, altura));
+    obstaculo.push(new componente("green", x, altura + vazio, 10, x - altura - vazio));
   }
 
-  //osbtaculos.atualizar();
-
-  for(i = 0; i < osbtaculos.length; i++) {
-    osbtaculos[i].x += -1;
-    osbtaculos[i].atualizar();
+  for(i = 0; i < obstaculo.length; i++){
+    obstaculo[i].x += -1;
+    obstaculo[i].atualiza();
   }
 
-  pontos.texto = "PONTOS: " + areaJogo.frame;
-  pontos.atualizar();
+  pontos.texto = "Pontos: " + areaJogo.frame;
+  pontos.atualiza();
   personagemObj.novaPosicao();
-  personagemObj.atualizar();
- 
-
+  personagemObj.atualiza();
 }
 
-
-function sobe(){
-  personagemObj.veloY -= 1;
+function subir(){
+  personagemObj.velocidadeY -= 1;
 }
 
-function desce(){
-  personagemObj.veloY += 1;
+function descer(){
+  personagemObj.velocidadeY += 1;
 }
 
-function right(){
-  personagemObj.veloX += 1;
+function direita(){
+  personagemObj.velocidadeX += 1;
 }
 
-function left(){
-  personagemObj.veloX -= 1;
+function esquerda(){
+  personagemObj.velocidadeX -= 1;
 }
 
-function pare() {
-  personagemObj.veloX = 0;
-  personagemObj.veloY = 0;
-}
-
-function reload() {
-location.reload();
+function parar(){
+  personagemObj.velocidadeX = 0;
+  personagemObj.velocidadeY = 0;
 }
